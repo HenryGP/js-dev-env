483b58c22bdd7b35e4f8cf861f1f4a56ae29410b
Henry Garnelo Prediger <egarnelo@enrique-macbookpro.local>
2017-09-04 20:26:37 +0100

    HTTP calls Module

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -11,6 +11,17 @@ var port = 3000;
 var app = express();
 var compiler = webpack(config);
 
+
+app.get("/users",function(req,res){
+  //Hardcoding an API call result for testing. Pretend this is hitting a real database
+  res.json([
+    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id":2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id":3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
