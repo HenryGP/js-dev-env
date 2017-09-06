20af073207ded84859770e5a4575fc13b41dcae4
Henry Garnelo Prediger <egarnelo@enrique-macbookpro.local>
2017-09-06 21:41:43 +0100

    Deploying static UI in Surge.

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -12,15 +12,6 @@ var app = express();
 app.use(compression());
 app.use(express.static('dist'));
 
-app.get("/users",function(req,res){
-  //Hardcoding an API call result for testing. Pretend this is hitting a real database
-  res.json([
-    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id":2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id":3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'../dist/index.html'));
 });
